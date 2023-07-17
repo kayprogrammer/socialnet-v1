567d13dab88d4d188330420c61df74fa1612887c
Kenechi Ifeanyi <kayprogrammer1@gmail.com>
2023-07-17 11:13:00 +0100

    chore: accounts models, managers, admin

--- apps/accounts/admin.py
+++ apps/accounts/admin.py
@@ -1,3 +1,93 @@
 from django.contrib import admin
+from django.contrib.auth.models import Group as DjangoGroup
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.admin import GroupAdmin as BaseGroupAdmin
+from django.utils.translation import gettext_lazy as _
 
-# Register your models here.
+from .models import User
+
+
+class Group(DjangoGroup):
+    class Meta:
+        verbose_name = "group"
+        verbose_name_plural = "groups"
+        proxy = True
+
+
+class GroupAdmin(BaseGroupAdmin):
+    pass
+
+
+class UserAdmin(BaseUserAdmin):
+    ordering = ["email"]
+
+    list_display = [
+        "id",
+        "first_name",
+        "last_name",
+        "email",
+        "is_staff",
+        "is_active",
+    ]
+
+    list_display_links = ["first_name", "last_name", "email"]
+    list_filter = [
+        "first_name",
+        "last_name",
+        "email",
+        "is_staff",
+        "is_active",
+    ]
+
+    fieldsets = (
+        (
+            _("Login Credentials"),
+            {"fields": ("email", "password")},
+        ),
+        (
+            _("Personal Information"),
+            {"fields": ("first_name", "last_name", "avatar")},
+        ),
+        (
+            _("Permissions and Groups"),
+            {
+                "fields": (
+                    "is_email_verified",
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                )
+            },
+        ),
+        (
+            _("Important Dates"),
+            {"fields": ("created_at", "updated_at", "last_login")},
+        ),
+    )
+
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": (
+                    "first_name",
+                    "last_name",
+                    "email",
+                    "password1",
+                    "password2",
+                    "is_staff",
+                    "is_active",
+                ),
+            },
+        ),
+    )
+    readonly_fields = ("created_at", "updated_at")
+    search_fields = ["first_name", "first_name", "email"]
+
+
+admin.site.register(User, UserAdmin)
+admin.site.register(Group, GroupAdmin)
+admin.site.unregister(DjangoGroup)
---
+++ apps/accounts/managers.py
@@ -0,0 +1,95 @@
+from django.contrib.auth.base_user import BaseUserManager
+from django.core.exceptions import ValidationError
+from django.core.validators import validate_email
+from django.utils.translation import gettext_lazy as _
+
+
+class CustomUserManager(BaseUserManager):
+    def email_validator(self, email):
+        try:
+            validate_email(email)
+        except ValidationError:
+            raise ValueError(_("You must provide a valid email address"))
+
+    def validate_user(self, first_name, last_name, email):
+        if not (first_name and last_name):
+            raise ValueError(_("Users must submit a first and last name"))
+
+        if email:
+            self.normalize_email(email)
+            self.email_validator(email)
+        else:
+            raise ValueError(_("Base User Account: An email address is required"))
+
+    def validate_superuser(self, password, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+        extra_fields.setdefault("is_active", True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError(_("Superusers must have is_staff=True"))
+
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError(_("Superusers must have is_superuser=True"))
+
+        if not password:
+            raise ValueError(_("Superusers must have a password"))
+
+        if email:
+            email = self.normalize_email(email)
+            self.email_validator(email)
+        else:
+            raise ValueError(_("Admin Account: An email address is required"))
+
+        return extra_fields
+
+    def create_user(self, first_name, last_name, email, password, **extra_fields):
+        self.validate_user(first_name, last_name, email)
+        user = self.model(
+            first_name=first_name, last_name=last_name, email=email, **extra_fields
+        )
+        user.set_password(password)
+        extra_fields.setdefault("is_staff", False)
+        extra_fields.setdefault("is_superuser", False)
+        user.save(using=self._db)
+        return user
+
+    async def acreate_user(
+        self, first_name, last_name, email, password, **extra_fields
+    ):
+        self.validate_user(first_name, last_name, email)
+        user = self.model(
+            first_name=first_name, last_name=last_name, email=email, **extra_fields
+        )
+
+        user.set_password(password)
+        extra_fields.setdefault("is_staff", False)
+        extra_fields.setdefault("is_superuser", False)
+        await user.asave(using=self._db)
+        return user
+
+    def create_superuser(self, first_name, last_name, email, password, **extra_fields):
+        extra_fields = self.validate_superuser(password, **extra_fields)
+        user = self.create_user(first_name, last_name, email, password, **extra_fields)
+        return user
+
+    async def acreate_superuser(
+        self, first_name, last_name, email, password, **extra_fields
+    ):
+        extra_fields = self.validate_superuser(password, **extra_fields)
+        user = await self.acreate_user(
+            first_name, last_name, email, password, **extra_fields
+        )
+        return user
+
+    def get_or_none(self, **kwargs):
+        try:
+            return self.get(**kwargs)
+        except self.model.DoesNotExist:
+            return None
+
+    async def get_or_none(self, **kwargs):
+        try:
+            return await self.aget(**kwargs)
+        except self.model.DoesNotExist:
+            return None
