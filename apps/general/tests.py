0753caba0dee9df64530bb20a80575f1041955b4
Kenechi Ifeanyi <kayprogrammer1@gmail.com>
2023-08-24 13:56:40 +0100

    tests: site detail

--- apps/general/tests.py
+++ apps/general/tests.py
@@ -1,3 +1,14 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+
+class TestGeneral(APITestCase):
+    sitedetail_url = "/api/v1/general/site-detail/"
+
+    def test_retrieve_sitedetail(self):
+        response = self.client.get(self.sitedetail_url)
+        self.assertEqual(response.status_code, 200)
+        result = response.json()
+        self.assertEqual(result["status"], "success")
+        self.assertEqual(result["message"], "Site Details fetched")
+        keys = ["name", "email", "phone", "address", "fb", "tw", "wh", "ig"]
+        self.assertTrue(all(item in result["data"] for item in keys))
