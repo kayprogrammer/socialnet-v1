652ad7285976a6d346673f95d3582b92be1e6090
Kenechi Ifeanyi <kayprogrammer1@gmail.com>
2023-08-26 00:36:27 +0100

    feed models

--- apps/feed/admin.py
+++ apps/feed/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
+from apps.feed.models import Comment, Post, Reaction, Reply
 
-# Register your models here.
+
+class ReactionAdmin(admin.ModelAdmin):
+    list_display = ("user", "rtype")
+    list_filter = ("user", "rtype")
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ("author", "slug", "created_at", "updated_at")
+    list_filter = ("author", "slug", "created_at", "updated_at")
+
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ("author", "created_at", "updated_at")
+    list_filter = ("author", "created_at", "updated_at")
+
+
+class ReplyAdmin(admin.ModelAdmin):
+    list_display = ("author", "created_at", "updated_at")
+    list_filter = ("author", "created_at", "updated_at")
+
+
+admin.site.register(Reaction, ReactionAdmin)
+admin.site.register(Post, PostAdmin)
+admin.site.register(Comment, CommentAdmin)
+admin.site.register(Reply, ReplyAdmin)
--- apps/feed/models.py
+++ apps/feed/models.py
@@ -1,3 +1,51 @@
+from autoslug import AutoSlugField
 from django.db import models
+from apps.accounts.models import User
+from django.utils.translation import gettext_lazy as _
+
+from apps.common.models import BaseModel, File
 
 # Create your models here.
+
+REACTION_CHOICES = (
+    ("LIKE", "LIKE"),
+    ("LOVE", "LOVE"),
+    ("HAHA", "HAHA"),
+    ("WOW", "WOW"),
+    ("SAD", "SAD"),
+    ("ANGRY", "ANGRY"),
+)
+
+
+class Reaction(BaseModel):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    rtype = models.CharField(max_length=20, choices=REACTION_CHOICES)
+
+
+def slugify_two_fields(self):
+    author = self.author
+    return f"{author.first_name}-{author.last_name}-{self.id}"
+
+
+class Post(BaseModel):
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts")
+    text = models.TextField()
+    slug = AutoSlugField(_("slug"), populate_from=slugify_two_fields, unique=True)
+    image = models.ForeignKey(File, on_delete=models.SET_NULL, null=True, blank=True)
+    reactions = models.ManyToManyField(Reaction, blank=True)
+
+
+class Comment(BaseModel):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
+    text = models.TextField()
+    reactions = models.ManyToManyField(Reaction, blank=True)
+
+
+class Reply(BaseModel):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    comment = models.ForeignKey(
+        Comment, on_delete=models.CASCADE, related_name="replies"
+    )
+    text = models.TextField()
+    reactions = models.ManyToManyField(Reaction, blank=True)
