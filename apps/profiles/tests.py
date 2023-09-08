4d056b1a24378b8f780bccc2d25e275f419ee427
Kenechi Ifeanyi <kayprogrammer1@gmail.com>
2023-09-09 00:42:28 +0100

    tests: update profile

--- apps/profiles/tests.py
+++ apps/profiles/tests.py
@@ -4,6 +4,7 @@ from apps.common.utils import TestUtil
 from apps.common.error import ErrorCode
 import uuid
 from cities_light.models import City, Country, Region
+from django.utils.text import slugify
 
 
 class TestProfile(APITestCase):
@@ -78,7 +79,7 @@ class TestProfile(APITestCase):
             },
         )
 
-        # Test for valid response for existent city name query
+        # Test for valid response for valid entry
         response = self.client.get(f"{self.profile_url}{user.username}/")
         self.assertEqual(response.status_code, 200)
         self.assertEqual(
@@ -97,6 +98,73 @@ class TestProfile(APITestCase):
                     "city": None,
                     "created_at": user.created_at.strftime("%Y-%m-%dT%H:%M:%S.%fZ"),
                     "updated_at": user.updated_at.strftime("%Y-%m-%dT%H:%M:%S.%fZ"),
+                },
+            },
+        )
+
+    def test_update_profile(self):
+        user = self.verified_user
+
+        user_data = {
+            "first_name": "TestUpdated",
+            "last_name": "VerifiedUpdated",
+            "bio": "Updated my bio",
+        }
+
+        # Test for valid response for non-existent username
+        response = self.client.patch(
+            f"{self.profile_url}invalid_username/", data=user_data, **self.bearer
+        )
+        self.assertEqual(response.status_code, 404)
+        self.assertEqual(
+            response.json(),
+            {
+                "status": "failure",
+                "message": "No user with that username",
+                "code": ErrorCode.NON_EXISTENT,
+            },
+        )
+
+        # Test for valid response for invalid owner
+        response = self.client.patch(
+            f"{self.profile_url}{user.username}/",
+            data=user_data,
+            **self.other_user_bearer,
+        )
+        self.assertEqual(response.status_code, 401)
+        self.assertEqual(
+            response.json(),
+            {
+                "status": "failure",
+                "message": "Not yours to edit",
+                "code": ErrorCode.INVALID_OWNER,
+            },
+        )
+
+        # Test for valid response for valid entry
+        response = self.client.patch(
+            f"{self.profile_url}{user.username}/", data=user_data, **self.bearer
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(
+            response.json(),
+            {
+                "status": "success",
+                "message": "User updated",
+                "data": {
+                    "first_name": user_data["first_name"],
+                    "last_name": user_data["last_name"],
+                    "username": slugify(
+                        f"{user_data['first_name']} {user_data['last_name']}"
+                    ),
+                    "email": user.email,
+                    "bio": user_data["bio"],
+                    "avatar": user.get_avatar,
+                    "dob": user.dob,
+                    "city": None,
+                    "created_at": mock.ANY,
+                    "updated_at": mock.ANY,
+                    "file_upload_data": None,
                 },
             },
         )
