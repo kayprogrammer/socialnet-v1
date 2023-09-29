7c7f66d99fc92c63458819a0cd330157b659c907
Kenechi Ifeanyi <kayprogrammer1@gmail.com>
2023-09-29 05:28:03 +0100

    chore: small cleanup

--- socialnet/settings/base.py
+++ socialnet/settings/base.py
@@ -16,8 +16,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config("SECRET_KEY")
 
-# SECURITY WARNING: don't run with debug turned on in production!
-
 ALLOWED_HOSTS = config("ALLOWED_HOSTS").split(" ")
 
 REST_FRAMEWORK = {
